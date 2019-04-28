1ec195232694bc961f79823f25fa34caf43f374b
Siqi <siqi@Siqis-MacBook-Air.local>
2019-04-27 20:35:58 -0400

    modify sentiment and regression of apple

--- webscrap.R
+++ webscrap.R
@@ -4,4 +4,26 @@ data=getReviews(281796108,'us',1)
 data
 View(data)
 
-orgindata=read.csv("AppleStore")
+orgindata=read.csv("AppleStore.csv")
+View(orgindata)
+
+reviews <- data.frame()
+
+df <- data.frame(orgindata)
+
+for (i in df[5811:nrow(df),2]){
+  t <- try(getReviews(i, 'us', 1))
+  if("try-error" %in% class(t)) print("error")
+  else {
+    print(i)
+    #id <- rep(i, nrow(getReviews(i, 'us', 1)))
+    review <- getReviews(i, 'us', 1)
+    review <- data.frame(i, review[["Review"]])
+    reviews <- rbind(reviews, review)
+    #reviews$id  <- rbind(reviews$id, rep(i, nrow(getReviews(i, 'us', 1))))
+  }
+}
+
+head(reviews)
+View(reviews)
+write.csv(reviews,'apple_reviews.csv')
